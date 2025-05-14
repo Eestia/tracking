cda2bc1b966520baafb6c975f21ed621f6eca67f
Dilara <dilaraozturk028@gmail.com>
2025-05-14 10:00:05 +0200

    responsive

--- src/components/unique/unique.jsx
+++ src/components/unique/unique.jsx
@@ -3,10 +3,11 @@ import './unique.css'
 export default function Unique(props) {
   return (
     <div className='profil'>
+    <div className='profil-top'>
       <img src={props.pfp} alt="" />
       <p id='report'>{props.report}</p>
       <h1 id='name'>{props.name}</h1>
-
+    </div>
       <div className='profil-in'>
         <div className='card-center'>
           <h1 id='daily' onClick={props.daily} style={{ cursor: 'pointer' }}>Daily</h1>
